5d8cad43eb3ab40814d264dae364e7cee2190baa
patracks9 <patricio_silva94@hotmail.com>
2025-08-17 18:34:27 -0400

    correccion puerto

--- app.py
+++ app.py
@@ -58,4 +58,4 @@ def delete_task(task_id):
 
 # Ejecuta la aplicación Flask
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=8080)
